d2054c41fb8611988821b996690200e0b603ba6a
Álvaro Rivas <alvarora12@gmail.com>
2023-06-23 00:02:22 +0200

    add service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,34 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+	vite: {
+		logLevel: 'info',
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		tailwind(),
+		mdx(),
+		image(),
+		AstroPWA({
+			mode: 'development',
+			base: '/',
+			scope: '/',
+			includeAssets: ['favicon.svg'],
+			selfDestroying: true,
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Terminal Portfolio by arialdev',
+				short_name: 'Terminal',
+			},
+			workbox: {
+				navigateFallback: '/404',
+				globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+			},
+		}),],
 });
